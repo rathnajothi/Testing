a50e29c211284b150fef8f9f3f5e9f4441351d47
Rathnajothi Perumalsamy <rathnajothi.p@gmail.com>
2017-11-24 14:53:30 +0530

    Added new files

---
+++ src/test/java/com/accenture/testscripts/TestMeScripts/AppTest.java
@@ -0,0 +1,21 @@
+package com.accenture.testscripts.TestMeScripts;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.annotations.Test;
+
+public class AppTest extends TestCase{
+	
+	public void testHome() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://localhost:8085/HelloWorld.war");
+		WebElement text = driver.findElement(By.xpath("html/body/h2"));
+		Assert.assertEquals(driver.getTitle(), "Welcome to Orchestrating with Jenkins training!!");
+		System.out.println("Test Passed");
+	}
+}
